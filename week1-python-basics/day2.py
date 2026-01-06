03c3137bbff1e8c882431de303a213c9d13c6fe7
aditi sharma <ADITI282020@gmail.com>
2026-01-06 13:44:29 +0530

    Day 2 python variable and data types completed

---
+++ week1-python-basics/day2.py
@@ -0,0 +1,58 @@
+# Variables and Data Types in Python
+'''''
+In Python, a variable is a name that stores data (a value) in the computer’s memory. It allows you to store, reuse, and modify data while a program is running.
+Unlike some other languages, Python does not require you to declare the type of a variable explicitly. The type is decided automatically based on the value assigned.
+'''''
+# Assigning values to variables
+age = 22              # integer variable
+name = "Aditi"        # string variable
+height = 5.4          # float variable
+is_student = True     # boolean variable
+
+# Printing variables
+print(age)
+print(name)
+print(height)
+print(is_student)
+
+
+# Data types in Python
+''''
+In Python, data types define the kind of value a variable can store. They tell Python how the data should be stored and what operations can be performed on it.
+'''''
+
+# Integer
+age = 25
+count = -10
+
+# Float
+price = 99.50
+height = 5.6
+
+# String
+name = "Aditi"
+city = 'Lucknow'
+
+# Boolean
+is_logged_in = True
+is_raining = False
+
+# list
+numbers = [1, 2, 3, 4]
+fruits = ["apple", "banana", "mango"]
+
+# Tuple
+coordinates = (10, 20)
+colors = ("red", "blue", "green")
+
+# Set
+unique_numbers = {1, 2, 3}
+
+# Dictionary
+student = {
+    "name": "Aditi",
+    "age": 22,
+    "course": "BTech"
+}
+
+
